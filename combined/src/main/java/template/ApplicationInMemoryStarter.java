27b320adffe2c4a8ece796f4f699e116910b32c5
Vladislav Girlea <vladislav.girlea@canoo.com>
2014-02-06 17:07:34 +0100

    init

--- combined/src/main/java/template/ApplicationInMemoryStarter.java
+++ combined/src/main/java/template/ApplicationInMemoryStarter.java
@@ -1,13 +1,18 @@
 package template;
 
-//import org.opendolphin.core.client.comm.JavaFXUiThreadHandler;
+import org.opendolphin.core.client.comm.UiThreadHandler;
 import org.opendolphin.core.comm.DefaultInMemoryConfig;
 
 public class ApplicationInMemoryStarter {
     public static void main(String[] args) throws Exception {
         DefaultInMemoryConfig config = new DefaultInMemoryConfig();
         config.registerDefaultActions();
-//        config.getClientDolphin().getClientConnector().setUiThreadHandler(new JavaFXUiThreadHandler());
+        config.getClientDolphin().getClientConnector().setUiThreadHandler(new UiThreadHandler() {
+            @Override
+            public void executeInsideUiThread(Runnable runnable) {
+
+            }
+        });
         registerApplicationActions(config);
         solrTest.Main.clientDolphin = config.getClientDolphin();
         javafx.application.Application.launch(solrTest.Main.class);
--- server/src/main/java/template/ApplicationAction.java
+++ server/src/main/java/template/ApplicationAction.java
@@ -3,22 +3,19 @@ package template;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.beans.Field;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.core.CoreContainer;
 import org.opendolphin.core.comm.Command;
+import org.opendolphin.core.comm.DataCommand;
 import org.opendolphin.core.server.action.DolphinServerAction;
 import org.opendolphin.core.server.comm.ActionRegistry;
 import org.opendolphin.core.server.comm.CommandHandler;
 import solrTest.Item;
 import solrTest.SolrService;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -48,7 +45,30 @@ public class ApplicationAction extends DolphinServerAction{
     public void registerIn(ActionRegistry actionRegistry) {
         actionRegistry.register("Query", new CommandHandler<Command>() {
             public void handleCommand(Command command, List<Command> response) {
+                long start = System.currentTimeMillis();
+                SolrService solrService = new SolrService();
+                try {
+                    solrServer = solrService.getSolrServer();
+                } catch (SolrServerException e) {
+                    e.printStackTrace();
+                }
+                SolrQuery solrQuery;
 
+                solrQuery = new SolrQuery("*:*");
+
+                solrQuery.setStart(0);                          //startign index
+                solrQuery.setRows(16000);                          //number of rows
+                List<Item> list = new ArrayList<Item>();
+                QueryResponse responses = null;
+                try {
+                    responses = getSolrServer().query(solrQuery);
+                } catch (SolrServerException e) {
+                }
+                SolrDocumentList results = responses.getResults();
+
+                for (SolrDocument result : results) {
+                    response.add(new DataCommand(new HashMap(1, Float.valueOf(result.getFieldValue("high").toString()))));
+                }
             }
         });
     }
